e514c6738553dd92f92f5388d1fdac54bc835208
Masdarul <m.rizqi1221@gmail.com>
2022-11-25 10:18:57 +0700

    initial commit

---
+++ src/notes.js
@@ -0,0 +1,10 @@
+const { addNoteHandler } = require('./handler');
+const notes = [
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: addNoteHandler,
+    },
+];
+
+module.exports = notes;
